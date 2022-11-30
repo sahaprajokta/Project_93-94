cf3a2d7ff169647737979e2687e66a12e8ab0d3c
sahaprajokta <97291708+sahaprajokta@users.noreply.github.com>
2022-11-30 22:36:15 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDQBUwOHL93o8Y81p63Kvopx_GCfiuIEaE",
+    authDomain: "class-test-68563.firebaseapp.com",
+    projectId: "class-test-68563",
+    storageBucket: "class-test-68563.appspot.com",
+    messagingSenderId: "411241788494",
+    appId: "1:411241788494:web:72dc18c71a30ef47a63f1f",
+    measurementId: "G-VT344BJX5P"
+};
